9443a8342f81e004475e30c84acfded7639694f6
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-12-17 19:26:22 +0700

    start

---
+++ microserviceNotes/src/test/java/com/example/Kuznetsov/microserviceNotes/MicroserviceNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kuznetsov.microserviceNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
